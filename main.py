2c42ac3a9c58bb8da9c256f62afd4a4ea676c32e
Rafiul islam <128176232+WD-Rafiul@users.noreply.github.com>
2023-04-14 22:13:45 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+# number / 3 = feez
+# number / 5 = buzz
+# number / 3,5 = feezbuzz
+number = int(input("Enter a number:"))
+if number % 3 == 0 and number % 5 == 0:
+    print("FezzBuzz")
+
+elif number % 3 == 0:
+    print("Feez")
+
+elif number % 5 == 0:
+    print("Buzz")
+
